61698331355a75a56f827f3309c1466ba9343b7b
Denis Moskovchenko <moskovchenkody@gmail.com>
2017-02-27 23:04:48 +0300

    первая версия запущена на отладчике

--- runserver.py
+++ runserver.py
@@ -20,38 +20,84 @@ class EntityType:
 
 
 class Entity:
-    pass
+    def __init__(self):
+        self.player = 0
 
 class FactoryEntity(Entity):
-    pass
+    def __init__(self):
+        super(FactoryEntity, self).__init__()
+        self.num_cyborg = 0
+        self.production = 0
 
 class TroopEntity(Entity):
-    pass
+    def __init__(self):
+        super(TroopEntity, self).__init__()
+        self.factory_from = 0
+        self.factory_to = 0
+        self.num_cyborg = 0
+        self.time_remain = 0
 
 class World:
     def __init__(self):
         """ Определяем список объектов доступных для класса """
         self.troops = []
-        self.factories = []
+        self.factories = {}
         self.links = []
+        self.num_factory = 0
 
     def init(self):
         """ инициализируем состояние игры """
-        factory_count = int(raw_input())  # the number of factories
+        self.num_factory = int(raw_input())  # the number of factories
         link_count = int(raw_input())  # the number of links between factories
 
+        self.factories = {a:None for a in xrange(self.num_factory)}
+
+
         # отображение вложенных данных
-        print >> sys.stderr, factory_count
+        print >> sys.stderr, self.num_factory
         print >> sys.stderr, link_count
 
-        self.create_links(factory_count)
+        self.create_links(self.num_factory)
         for i in xrange(link_count):
             raw = raw_input()
             print >> sys.stderr, raw
             factory_1, factory_2, distance = [int(j) for j in raw.split()]
 
             # определение данных для отображение
-            w.links[factory_1][factory_2] = distance
+            self.links[factory_1][factory_2] = distance
+
+    def update(self):
+        """Обновление статуса текущего мира"""
+        entity_count = int(raw_input())  # the number of entities (e.g. factories and troops)
+
+        self.troops = []
+
+        for i in xrange(entity_count):
+            raw = raw_input()
+            print >> sys.stderr, raw
+            entity_id, entity_type, arg_1, arg_2, arg_3, arg_4, arg_5 = raw.split()
+            entity_id = int(entity_id)
+            arg_1 = int(arg_1)
+            arg_2 = int(arg_2)
+            arg_3 = int(arg_3)
+            arg_4 = int(arg_4)
+            arg_5 = int(arg_5)
+
+            if entity_type == EntityType.FACTORY:
+                factory = self.factories[entity_id]
+                # инициализируем фабрику
+                if factory is None:
+                    factory = FactoryEntity()
+                    self.factories[entity_id] = factory
+
+                factory.player, factory.num_cyborg, factory.production = arg_1, arg_2, arg_3
+
+
+            elif entity_type == EntityType.TROOP:
+                troop = TroopEntity()
+                troop.player, troop.factory_from, troop.factory_to, troop.num_cyborg,\
+                troop.time_remain = arg_1, arg_2, arg_3, arg_4, arg_5
+                self.troops.append(troop)
 
 
 
@@ -68,78 +114,24 @@ class World:
 # Описание стратегии для принятия решения о базе
 
 class Strategy:
-    
-    def __init__(self, world):        
+    """ Применяемая стратегия """
+    def __init__(self, world):
         self.world = world
-        
-    
-    def get_player_command():
+
+    def get_player_command(self):
         return "WAIT"
 
 
-# Описание генератора мира для определения заданных объектов 
-
-class WorldGenerator:
-
-    @staticmethod
-    def get_world():
-        
-        w = World()
-
-        factory_count = int(raw_input())  # the number of factories
-        link_count = int(raw_input())  # the number of links between factories
-
-        # отображение вложенных данных
-        print >> sys.stderr, factory_count
-        print >> sys.stderr, link_count
-
-        w.create_links(factory_count)
-        for i in xrange(link_count):
-            raw = raw_input()
-            print >> sys.stderr, raw
-            factory_1, factory_2, distance = [int(j) for j in raw.split()]
-
-            # определение данных для отображение
-            w.links[factory_1][factory_2] = distance
-
-        return w
-
-    @staticmethod
-    def read_entities():
-        entity_count = int(raw_input())  # the number of entities (e.g. factories and troops)
-
-        for i in xrange(entity_count):
-            raw = raw_input()
-            print >> sys.stderr, raw
-            entity_id, entity_type, arg_1, arg_2, arg_3, arg_4, arg_5 = raw.split()
-            entity_id = int(entity_id)
-            arg_1 = int(arg_1)
-            arg_2 = int(arg_2)
-            arg_3 = int(arg_3)
-            arg_4 = int(arg_4)
-            arg_5 = int(arg_5)
-
-            if entity_type == EntityType.FACTORY:
-                pass
-            elif entity_type == EntityType.TROOP:
-                pass    
-            pass
-
-    @staticmethod
-    def get_factory(entity_id, entity_type, arg_1, arg_2, arg_3):
-        return None
-
-    @staticmethod
-    def get_troop(entity_id, entity_type, arg_1, arg_2, arg_3, arg_4, arg_5):
-        return None
-        
-
 # основная часть программы
 
-w = World()
+world = World()
 
-w.init()
+world.init()
 
 while True:
 
-    w.update()
+    world.update()
+    strategy = Strategy(world)
+    print strategy.get_player_command()
+
+    
